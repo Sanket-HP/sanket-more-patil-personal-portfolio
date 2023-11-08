82f21621854bcf9e5b0608f03b87be09e0b1d1a4
Sanket Sarjerao More-Patil <sp3130016@gmail.com>
2023-11-08 23:12:18 +0530

    Create about.jsx

---
+++ src/components/about.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    <section className="about">
+      <div className="heading">About Me</div>
+      <div className="container">
+        <div className="image-container"></div>
+        <div className="info">
+          Illo saepe, doloribus omnis facere animi iste, dolorum reprehenderit
+          aliquid perferendis numquam laborum fuga ipsum harum fugit vitae
+          obcaecati magni tempora labore cupiditate ab facilis eum repellendus
+          nostrum cumque. Porro delectus nobis atque omnis laborum ipsam impedit
+          fugit quas laudantium totam, commodi voluptas, mollitia nihil itaque
+          facere eaque nam nulla.
+        </div>
+      </div>
+    </section>
+  );
+}
